f08cd5e4f060d7645de94a19a74a2ac1c29be9a6
Levan Salia <levanisalia@yahoo.com>
2021-01-17 20:45:20 +0200

    modify run file to initialize ETL call

--- run.py
+++ run.py
@@ -1,20 +1,10 @@
-import psycopg2
 from time import sleep
 
-sleep(10)
-try:
-    # Just some sample code.
-    with psycopg2.connect(user='user',
-                          password='password',
-                          host='postgres',
-                          database='database') as connection:
-        cursor = connection.cursor()
-        # Print PostgreSQL Connection properties
-        print(connection.get_dsn_parameters(), '\n')
-        
-        # Print PostgreSQL version
-        cursor.execute('SELECT version();')
-        record = cursor.fetchone()
-        print('You are connected to - ', record, '\n')
-except (Exception, psycopg2.Error) as error:
-    print('Error while connecting to PostgreSQL', error)
+from etl.etl import ETL
+
+if __name__ == '__main__':
+
+    sleep(10)
+
+    processor = ETL()
+    processor.run()
